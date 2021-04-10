4ff5074077f692eed1bf2522dd38e77977d5c3b2
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-04-10 18:46:49 -0500

    got it working except for delete

--- server.js
+++ server.js
@@ -16,7 +16,24 @@ app.get("/api/notes/:id", function(req, res) {
 app.get("/notes", function(req,res){
     res.sendFile(path.join(mainDir, "notes.html"));
 });
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+});
 
 app.listen(PORT, function() {
     console.log(`http://localhost:${PORT}`);
-})
+});
